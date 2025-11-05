b946b2929a78a72d317b032bb75e81cc127504c5
jaime <164238589+jvime13@users.noreply.github.com>
2025-11-05 14:41:37 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Acta Digital")
+st.subheader("Aplicación creada en Streamlit")
+st.write("¡Bienvenido! Aquí podrás registrar y visualizar actas de manera digital.")
